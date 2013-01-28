392d77803754151a549bf96f34d795b40284c892
Peter Hajas <peterhajas@gmail.com>
2013-01-28 01:19:20 -0800

    Fixed some sphere.c compile errors.

--- source/sphere.c
+++ source/sphere.c
@@ -7,21 +7,21 @@ roots intersection_with_ray(sphere s, ray r)
 	// We need r in sphere-coordinates
 
 	ray r_sphere_coordinates;
-	s_transform_inverse = inverse_of_matrix(s.transform);
+	matrix s_transform_inverse = inverse_of_matrix(s.transform);
 	r_sphere_coordinates.point     = transform_point(s_transform_inverse, r.point);
-	r_sphere_coordinates.direction = transform_direction(s_transform_inverse, r.direction);
+	r_sphere_coordinates.direction = transform_ray(s_transform_inverse, r.direction);
 
-	vector point_minus_center = subtract_vector(r_sphere_coordinates.point, s.center);
+	vector point_minus_center = subtract_vectors(r_sphere_coordinates.point, s.center);
 
-	float s[3];
+	float polynomials[3];
 
-	s[2] = square_of_magnitude_of_vector(r_sphere_coordinates.direction);
-	s[1] = 2 * dot_product(point_minus_center, r_sphere_coordinates.direction);
-	s[0] = square_of_magnitude_of_vector(point_minus_center) - s.radius * radius;
+	polynomials[2] = square_of_magnitude_of_vector(r_sphere_coordinates.direction);
+	polynomials[1] = 2 * dot_product(point_minus_center, r_sphere_coordinates.direction);
+	polynomials[0] = square_of_magnitude_of_vector(point_minus_center) - s.radius * s.radius;
 
 	roots quad_roots;
 
-	quad_roots = quadratic(s[2],s[1],s[0]);
+	quad_roots = quadratic(polynomials[2], polynomials[1], polynomials[0]);
 
 	return quad_roots;
 }
@@ -29,15 +29,15 @@ roots intersection_with_ray(sphere s, ray r)
 bool point_lies_inside(sphere s, vector point)
 {
 	vector point_sphere_coordinates;
-	s_transform_inverse = inverse_of_matrix(s.transform);
+	matrix s_transform_inverse = inverse_of_matrix(s.transform);
 
 	point_sphere_coordinates = transform_point(s_transform_inverse, point);
 	
 	float x = point_sphere_coordinates.x;
 	float y = point_sphere_coordinates.y;
-	float c = point_sphere_coordinates.z;
+	float z = point_sphere_coordinates.z;
 
-	if((x * x + y * y + z * z) < radius * radius)
+	if((x * x + y * y + z * z) < s.radius * s.radius)
 	{
 		return true;
 	}
